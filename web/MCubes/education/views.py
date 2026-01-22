827dae90e65e4a8c1d4b76e5db0adfaec2232601
Egor-Windows <227mr123@mail.ru>
2026-01-22 23:41:05 +0300

    a lot of

--- web/MCubes/education/views.py
+++ web/MCubes/education/views.py
@@ -5,3 +5,12 @@ def choose_cube(request):
 
 def course_2x2_def_1(request):
     return render(request, 'education/course_2x2_def_1.html')
+
+def course_2x2_def_2(request):
+    return render(request, 'education/course_2x2_def_2.html')
+
+def course_2x2_def_3(request):
+    return render(request, 'education/course_2x2_def_3.html')
+
+def course_2x2_def_4(request):
+    return render(request, 'education/course_2x2_def_4.html')
